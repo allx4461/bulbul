ed1d6f9afd90aa8716436d167c3bdf4272ca1a68
allx4461 <137797438+allx4461@users.noreply.github.com>
2025-12-23 21:37:16 +0300

    господи когда уже

--- entity.h
+++ entity.h
@@ -5,4 +5,5 @@ public:
     virtual ~Entity() =default;//киллить наследников
     virtual std::vector<int> getinfo()=0;//возвращает вектор из полей
     virtual std::vector<char> getimage()=0;//возвращает спрайт
-    virtual void update()=0;};
+    virtual void update(World& world)=0;
+    virtual void draw(Canvas& canvas)=0;};
